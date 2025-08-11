c5563ee23e0b61f4b39b90c0c1700a3db227baee
swpark192 <sw031489@naver.com>
2025-08-11 15:42:52 +0900

    Added main solution
    Added main solution

---
+++ GameAssembly/GameScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameAssembly
+{
+    public class MonoBehaviour
+    {
+        public virtual void Start() { }
+        public virtual void Update() { }
+    }
+
+    public class Player : MonoBehaviour
+    {
+        public override void Start()
+        {
+            System.Console.WriteLine("Player Start!");
+        }
+
+        public override void Update()
+        {
+            System.Console.WriteLine("Player Update!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MonoScriptingDemo
-A minimal C++ project demonstrating embedding Mono runtime for C# scripting integration, with Start/Update lifecycle simulation. C++에서 Mono 런타임을 임베딩하여 C# 스크립팅을 실행하는 간단한 데모 프로젝트
+A minimal C++ project demonstrating embedding Mono runtime for C# scripting integration, with Start/Update lifecycle simulation.
+
+C++에서 Mono 런타임을 임베딩하여 C# 스크립팅을 실행하는 간단한 데모 프로젝트
